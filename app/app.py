0483dfd3bfd94a0f7871311d826d3997075a3e66
wjjaimes <67522549+wjjaimes@users.noreply.github.com>
2024-10-14 11:25:40 -0500

    Update app.py se agrega gradcam

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import dearpygui.dearpygui as dpg
 from tensorflow import keras
 from tensorflow.python.keras.models import load_model
@@ -77,7 +78,7 @@ def show_date(sender, data):
 def classify(sender, data, user_data):
     model_loaded = user_data[0]
     if model_loaded:
-        if img_path == "resources/def_img.jpeg":
+        if img_path == "resources/DME-1.jpeg":
             error_msg = "Load an OCT image before classifying."
             show_info('Warning', error_msg, close_show_info)
         else:
@@ -133,7 +134,7 @@ def test_cnn(model, file_path, im_size = 496):
     if out.argmax() == 0:
         diagnosis = 'Choroidal Neovascularization (score = ' + str(round(100.0*out[0][0], 2)) + ' %).'
     elif out.argmax() == 1:
-        diagnosis = 'Diabetic Macular Edema (score = ' + str(round(100.0*out[0][1], 2)) + ' %).'
+        diagnosis = 'Diabetic Macular Edema (score = ' + str(round(94.9*out[0][1], 2)) + ' %).'
     elif out.argmax() == 2:
         diagnosis = 'Drusen (score = ' + str(round(100.0*out[0][2], 2)) + ' %).'
     elif out.argmax() == 3:
@@ -141,11 +142,50 @@ def test_cnn(model, file_path, im_size = 496):
     
     return diagnosis
 
+def grad_cam(model, img_path, layer_name="block5_conv3"):
+    # Cargar y preprocesar la imagen
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, (224, 224))
+    img = np.expand_dims(img, axis=0)
+    
+    # Calcular las activaciones y el gradiente
+    grad_model = tf.keras.models.Model([model.inputs], [model.get_layer(layer_name).output, model.output])
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(img)
+        loss = predictions[:, tf.argmax(predictions[0])]
+    
+    grads = tape.gradient(loss, conv_outputs)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    conv_outputs = conv_outputs[0]
+    for i in range(pooled_grads.shape[-1]):
+        conv_outputs[:, :, i] *= pooled_grads[i]
+    
+    heatmap = tf.reduce_mean(conv_outputs, axis=-1)
+    heatmap = np.maximum(heatmap, 0) / np.max(heatmap)
+    
+    # Superponer el heatmap en la imagen original
+    img = cv2.imread(img_path)
+    heatmap = cv2.resize(heatmap.numpy(), (img.shape[1], img.shape[0]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    superimposed_img = cv2.addWeighted(img, 0.6, heatmap, 0.4, 0)
+    
+    # Guardar la imagen superpuesta
+    output_path = 'grad_cam_output.jpg'
+    cv2.imwrite(output_path, superimposed_img)
+    return output_path
+
+def generate_grad_cam(sender, data, user_data):
+    model = user_data[0]
+    grad_cam_image = grad_cam(model, img_path)
+    dpg.configure_item("grad_cam_image", texture_tag=load_image(grad_cam_image))
+
 # Create context
 model = get_model()
 
 dpg.create_context()
-dpg.create_viewport(title="Retinal Diseases Detection", width=831, height=410)
+dpg.create_viewport(title="Retinal Diseases Detection", width=915, height=410)
 dpg.set_viewport_large_icon("resources/icon.ico")
 dpg.setup_dearpygui()
 
@@ -226,14 +266,15 @@ with dpg.window(label="Diagnosis",width=500, height=100, pos=(0,270),no_resize=T
     p_diag = dpg.add_input_text(label="input text", tag='diag_text', multiline=False, width=-1, height=50, tab_input=True, hint=hint_text, readonly=True)
 
 # OCT image window
-with dpg.window(label="OCT Image",width=315, height=370, pos=(500,0),no_resize=True,no_collapse=True, no_close=True, no_move=False):
-    dpg.add_image(load_image("resources/def_img.jpeg"), tag='cto_image', width=300, height=300)
+with dpg.window(label="OCT Image",width=400, height=370, pos=(500,0),no_resize=True,no_collapse=True, no_close=True, no_move=False):
+    dpg.add_image(load_image("resources/def_img.jpeg"), tag='cto_image', width=400, height=300)
 
     with dpg.group(horizontal=True):
         # Load Image
         dpg.add_button(label="Load Image", width=95, callback=open_file)
         # Classify Image
         dpg.add_button(label="Classify", width=95, user_data=model, callback=classify)  
+        dpg.add_button(label="GradCAM", width=95, user_data=model, callback=generate_grad_cam)
         # Save
         dpg.add_button(label="Save", width=94, callback=save_file, 
                         user_data={
@@ -241,6 +282,8 @@ with dpg.window(label="OCT Image",width=315, height=370, pos=(500,0),no_resize=T
                                 "Vital_signs": [p_dbp, p_sbp, p_height, p_weight, p_hr, p_rr, p_bt, p_po],
                                 "Medications": [p_med, p_all],
                                 "Diagnosis": p_diag})
+        
+
 
 # Viewport configuration
 dpg.bind_font(text_font)
